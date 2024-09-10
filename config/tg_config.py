5adce75da5db4f88857891343fea9d73672af579
Goddess <goddess@example.com>
2024-09-10 20:51:39 +0000

    delete configs

--- config/tg_config.py
+++
@@ -1,7 +0,0 @@
-TG_CONFIG = {
-    "token": "6942262495:AAHeye7xtoEJ3M0skfOI8OikPXzCtvdlLHM",
-    "client": "https://api.telegram.org/bot6942262495:AAHeye7xtoEJ3M0skfOI8OikPXzCtvdlLHM/",
-    # "channel_id": "-1002132134413",
-    # "archive": {"hyppomania": "-1002132134413"},
-    "channel_id": "-1001995645080",
-}
