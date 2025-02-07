9f5abf4a846c79917e3f070a5cfb4e49ca4707bc
Goddess <goddess@example.com>
2025-02-07 10:01:32 +0000

    del unnecessary files

--- announce.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source bots/bin/activate
-python DiscordBot.py script
--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3.11 -m venv bots
-echo "Виртуальная среда создана"
-source "bots/bin/activate"
-echo "Виртуальная среда активирована"
-pip3 install -r requirements.txt
-echo "Установлены зависимости"
-bash announce.sh
